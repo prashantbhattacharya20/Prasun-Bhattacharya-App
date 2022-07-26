583518b390b590f3d5c4fbc1d8c513b6b418b643
Prashant Bhattacharya <prashantbhattacharya72@gmail.com>
2022-07-26 20:24:27 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    int days = 30;
+    String name = "Prashant";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catlog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days Days of Flutter Mr. $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
